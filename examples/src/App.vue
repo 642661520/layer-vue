1f1d63f25e7b73428f782c154260bd946cdbdc32
finalsummer <642661520de@gmail.com>
2021-05-07 16:48:22 +0800

    up

---
+++ examples/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <VueLayer></VueLayer>
+  </div>
+</template>
+
+<script>
+import VueLayer from "../../src/lib/vuelayer.vue";
+export default {
+  components: {
+    VueLayer,
+  },
+  name: "app",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style></style>
