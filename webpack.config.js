44f375ff01f3c003808c45aea28d797378b052fd
finalsummer <642661520de@gmail.com>
2021-08-08 15:10:17 +0800

    docs

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 var path = require("path");
 var webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./examples/main.js",
   output: {
     filename:'bundle.js',
-    path: path.resolve(__dirname, "./bulid")
+    path: path.resolve(__dirname, "./docs")
   },
   module: {
     rules: [
@@ -58,7 +59,12 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: "#eval-source-map"
+  devtool: "#eval-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+  ]
 };
 
 if (process.env.NODE_ENV === "production") {
