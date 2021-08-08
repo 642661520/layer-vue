a58b0114fe6362e4d526a5b59e91601906c672ca
finalsummer <642661520de@gmail.com>
2021-08-08 15:02:03 +0800

    up

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,16 @@
 var path = require("path");
 var webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  // entry: './src/index.js',
   entry: "./examples/main.js",
   output: {
     filename:'bundle.js',
     path: path.resolve(__dirname, "./bulid")
   },
-  // output: {
-  //   path: path.resolve(__dirname, "./lib"),
-  //   publicPath: "/lib/",
-  //   filename: "layer-vue.js",
-  //   library: "LayerVue", // library指定的就是你使用require时的模块名，这里便是require("LayerVue")
-  //   libraryTarget: "umd", //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
-  //   umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
-  // },
   module: {
     rules: [
       {
         test: /\.css$/,
-        // use: ExtractTextPlugin.extract({
-        //   fallback: "vue-style-loader",
-        //   use:"css-loader"
-        // })
-        // use: ExtractTextPlugin.extract(["vue-style-loader", "css-loader"])
         use: ["vue-style-loader", "css-loader"]
       },
       {
@@ -73,12 +58,7 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: "#eval-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
-  ]
+  devtool: "#eval-source-map"
 };
 
 if (process.env.NODE_ENV === "production") {
