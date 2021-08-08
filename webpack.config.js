673f0ee0ba5d0bdc45aa3713552f80c283840fbf
finalsummer <642661520de@gmail.com>
2021-08-08 14:28:36 +0800

    add testpage

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
 var path = require("path");
 var webpack = require("webpack");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: './src/index.js',
-  // entry: "./examples/main.js",
+  // entry: './src/index.js',
+  entry: "./examples/main.js",
   output: {
-    path: path.resolve(__dirname, "./lib"),
-    publicPath: "/lib/",
-    filename: "layer-vue.js",
-    library: "LayerVue", // library指定的就是你使用require时的模块名，这里便是require("LayerVue")
-    libraryTarget: "umd", //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
-    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+    filename:'bundle.js',
+    path: path.resolve(__dirname, "./bulid")
   },
+  // output: {
+  //   path: path.resolve(__dirname, "./lib"),
+  //   publicPath: "/lib/",
+  //   filename: "layer-vue.js",
+  //   library: "LayerVue", // library指定的就是你使用require时的模块名，这里便是require("LayerVue")
+  //   libraryTarget: "umd", //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
+  //   umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+  // },
   module: {
     rules: [
       {
@@ -68,10 +73,24 @@ module.exports = {
   performance: {
     hints: false
   },
-  devtool: "#eval-source-map"
+  devtool: "#eval-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+  ]
 };
 
 if (process.env.NODE_ENV === "production") {
+  module.exports.entry='./src/index.js',
+  module.exports.output={
+    path: path.resolve(__dirname, "./lib"),
+    publicPath: "/lib/",
+    filename: "layer-vue.js",
+    library: "LayerVue", // library指定的就是你使用require时的模块名，这里便是require("LayerVue")
+    libraryTarget: "umd", //libraryTarget会生成不同umd的代码,可以只是commonjs标准的，也可以是指amd标准的，也可以只是通过script标签引入的。
+    umdNamedDefine: true // 会对 UMD 的构建过程中的 AMD 模块进行命名。否则就使用匿名的 define。
+  },
   module.exports.devtool = "#none";
   module.exports.module.rules[0]={
     test: /\.css$/,
@@ -80,7 +99,7 @@ if (process.env.NODE_ENV === "production") {
       use:"css-loader"
     })
   }
-  module.exports.plugins = (module.exports.plugins || []).concat([
+  module.exports.plugins = [
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: '"production"'
@@ -96,5 +115,5 @@ if (process.env.NODE_ENV === "production") {
       minimize: true
     }),
     new ExtractTextPlugin("index.css")
-  ]);
+  ]
 }
